dc7f8629feee4b0418f078d117b28badbe6302ca
Dawen Liang <dl2771@columbia.edu>
2014-12-09 02:08:17 -0500

    fixed sparse matrix bug

--- code/nbmf.py
+++ code/nbmf.py
@@ -10,19 +10,19 @@ CREATED: 2014-11-18 20:50:49 by Dawen Liang <dliang@ee.columbia.edu>
 import sys
 import numpy as np
 from scipy import sparse, special
-
-from math import log
 from sklearn.base import BaseEstimator, TransformerMixin
 
 
 DEFAULT_NUM_ITER = 1
+DEFAULT_MIN_ITER = 10
 EPS = np.spacing(1)
 
 
 class NegBinomMF(BaseEstimator, TransformerMixin):
     ''' Negative-binomial matrix factorization with batch inference '''
     def __init__(self, n_components=100, max_iter=100, max_inner_iter=20,
-                 tol=0.0005, smoothness=100, random_state=None, verbose=False,
+                 tol=0.0001, smoothness=100, init_flat_lam=True,
+                 random_state=None, verbose=False,
                  **kwargs):
         ''' Negative-binomial matrix factorization
         Arguments
@@ -49,6 +49,7 @@ class NegBinomMF(BaseEstimator, TransformerMixin):
         self.max_inner_iter = max_inner_iter
         self.tol = tol
         self.smoothness = smoothness
+        self.init_flat_lam = init_flat_lam
         self.random_state = random_state
         self.verbose = verbose
 
@@ -65,47 +66,49 @@ class NegBinomMF(BaseEstimator, TransformerMixin):
         self.c = float(kwargs.get('c', 0.1))
         self.d = float(kwargs.get('d', 0.1))
 
-        self.r = float(kwargs.get('r', 2.0))
+        self.r = float(kwargs.get('r', 1.0))
 
     def _init_aux(self, X):
         # variational parameters for lambda
         self.nu_lam = self.smoothness * np.random.gamma(self.smoothness,
                                                         1. / self.smoothness,
-                                                        size=X.shape)
-        #self.rho_lam = 1. / (X + EPS) * self.smoothness * \
-        #    np.random.gamma(self.smoothness, 1. / self.smoothness,
-        #                    size=X.shape)
-        self.rho_lam = self.smoothness * np.random.gamma(self.smoothness,
-                                                         1. / self.smoothness,
-                                                         size=X.shape)
+                                                        size=X.shape
+                                                        ).astype(np.float32)
+        if self.init_flat_lam:
+            self.rho_lam = self.smoothness * \
+                np.random.gamma(self.smoothness, 1. / self.smoothness,
+                                size=X.shape).astype(np.float32)
+        else:
+            self.rho_lam = 1. / (X + EPS) * self.smoothness * \
+                np.random.gamma(self.smoothness, 1. / self.smoothness,
+                                size=X.shape).astype(np.float32)
+
         self.Elam, self.Eloglam = comp_gamma_expectations(self.nu_lam,
                                                           self.rho_lam)
 
     def _init_users(self, n_users):
         # variational parameters for theta
-        self.rho_t = 10000 * np.random.gamma(self.smoothness,
-                                             1. / self.smoothness,
-                                             size=(self.n_components, n_users))
-        self.tau_t = 10000 * np.random.gamma(self.smoothness,
-                                             1. / self.smoothness,
-                                             size=(self.n_components, n_users))
-        self.Et, self.Etinv = comp_gig_expectations(self.a,
-                                                    self.rho_t,
-                                                    self.tau_t)
-        self.Etinvinv = 1. / self.Etinv
+        self.nu_t = self.smoothness * \
+            np.random.gamma(self.smoothness, 1. / self.smoothness,
+                            size=(self.n_components, n_users)
+                            ).astype(np.float32)
+        self.rho_t = self.smoothness * \
+            np.random.gamma(self.smoothness, 1. / self.smoothness,
+                            size=(self.n_components, n_users)
+                            ).astype(np.float32)
+        self.Et, self.Elogt = comp_gamma_expectations(self.nu_t, self.rho_t)
 
     def _init_items(self, n_items):
         # variational parameters for beta
-        self.rho_b = 10000 * np.random.gamma(self.smoothness,
-                                             1. / self.smoothness,
-                                             size=(n_items, self.n_components))
-        self.tau_b = 10000 * np.random.gamma(self.smoothness,
-                                             1. / self.smoothness,
-                                             size=(n_items, self.n_components))
-        self.Eb, self.Ebinv = comp_gig_expectations(self.c,
-                                                    self.rho_b,
-                                                    self.tau_b)
-        self.Ebinvinv = 1. / self.Ebinv
+        self.nu_b = self.smoothness * \
+            np.random.gamma(self.smoothness, 1. / self.smoothness,
+                            size=(n_items, self.n_components)
+                            ).astype(np.float32)
+        self.rho_b = self.smoothness * \
+            np.random.gamma(self.smoothness, 1. / self.smoothness,
+                            size=(n_items, self.n_components)
+                            ).astype(np.float32)
+        self.Eb, self.Elogb = comp_gamma_expectations(self.nu_b, self.rho_b)
 
     def fit(self, X):
         '''Fit the model to the data in X.
@@ -136,8 +139,6 @@ class NegBinomMF(BaseEstimator, TransformerMixin):
         if attr is None:
             attr = 'Eb'
 
-        if sparse.isspmatrix(X):
-            X = X.toarray()
         self._init_aux(X)
         if idx is None:
             self._init_items(n_items)
@@ -151,9 +152,9 @@ class NegBinomMF(BaseEstimator, TransformerMixin):
             old_inner_bd = -np.inf
             for j in xrange(self.max_inner_iter):
                 if update_users:
-                    self._update_users()
-                self._update_items(idx=idx)
-                inner_bound = self._bound(X, aux_updated=False, idx=idx)
+                    self._update_users(X)
+                self._update_items(X, idx=idx)
+                inner_bound = self._bound(X, idx=idx)
                 improvement = (inner_bound - old_inner_bd) / abs(old_inner_bd)
                 if self.verbose:
                     sys.stdout.write('\r\tInner TERATION: %d\tObjective: %.2f'
@@ -162,11 +163,13 @@ class NegBinomMF(BaseEstimator, TransformerMixin):
                                                             old_inner_bd,
                                                             improvement))
                     sys.stdout.flush()
+                if improvement < self.tol and j >= DEFAULT_MIN_ITER:
+                    break
                 old_inner_bd = inner_bound
             if self.verbose:
                 sys.stdout.write('\n')
-
             self._update_aux(X, idx=idx)
+
             bound = self._bound(X, idx=idx)
             improvement = (bound - old_bd) / abs(old_bd)
             if self.verbose:
@@ -174,70 +177,63 @@ class NegBinomMF(BaseEstimator, TransformerMixin):
                       'Old objective: %.2f\t'
                       'Improvement: %.5f' % (i, bound, old_bd, improvement))
                 sys.stdout.flush()
-            if improvement < self.tol and improvement > 10:
+            if improvement < self.tol and i >= DEFAULT_MIN_ITER:
                 break
             old_bd = bound
         pass
 
-    def _update_users(self):
-        EXinv = 1. / self.Eb.dot(self.Et)
-        laminvXsq = self.Elam / (self.Ebinvinv.dot(self.Etinvinv))**2
-        self.rho_t = self.b + self.r * self.Eb.T.dot(EXinv)
-        self.tau_t = self.r * self.Etinvinv**2 * self.Ebinvinv.T.dot(laminvXsq)
-        self.tau_t[self.tau_t < 1e-100] = 0
-        self.Et, self.Etinv = comp_gig_expectations(self.a,
-                                                    self.rho_t,
-                                                    self.tau_t)
-        self.Etinvinv = 1. / self.Etinv
+    def _update_users(self, X):
+        ratio = X / self._xexplog()
+        self.nu_t = self.a + np.exp(self.Elogt) * np.exp(self.Elogb.T).dot(ratio)
+        self.rho_t = self.b + self.Eb.T.dot(self.Elam)
+        self.Et, self.Elogt = comp_gamma_expectations(self.nu_t, self.rho_t)
 
-    def _update_items(self, idx=None):
+    def _update_items(self, X, idx=None):
         if idx is None:
             idx = slice(None, None)
-        EXinv = 1. / self.Eb[idx].dot(self.Et)
-        laminvXsq = self.Elam / (self.Ebinvinv[idx].dot(self.Etinvinv))**2
-        rho_b = self.d + self.r * EXinv.dot(self.Et.T)
-        tau_b = self.r * self.Ebinvinv[idx]**2 * laminvXsq.dot(self.Etinvinv.T)
-        tau_b[tau_b < 1e-100] = 0
-        self.rho_b[idx], self.tau_b[idx] = rho_b, tau_b
-        self.Eb[idx], self.Ebinv[idx] = comp_gig_expectations(self.c,
-                                                              rho_b,
-                                                              tau_b)
-        self.Ebinvinv[idx] = 1. / self.Ebinv[idx]
+        ratio = X / self._xexplog(idx=idx)
+        nu_b = self.c + np.exp(self.Elogb[idx]) * ratio.dot(np.exp(self.Elogt.T))
+        rho_b = self.d + self.Elam.dot(self.Et.T)
+        self.nu_b[idx], self.rho_b[idx] = nu_b, rho_b
+        self.Eb[idx], self.Elogb[idx] = comp_gamma_expectations(nu_b, rho_b)
 
     def _update_aux(self, X, idx=None):
         if idx is None:
             idx = slice(None, None)
         self.nu_lam = self.r + X
-        self.rho_lam = 1 + self.r / self.Ebinvinv[idx].dot(self.Etinvinv)
+        self.rho_lam = self.r + self.Eb[idx].dot(self.Et)
         self.Elam, self.Eloglam = comp_gamma_expectations(self.nu_lam,
                                                           self.rho_lam)
 
-    def _bound(self, X, aux_updated=True, idx=None):
+    def _xexplog(self, idx=None):
+        '''
+        sum_k exp(E[log theta_{ik} * beta_{kd}])
+        '''
+        if idx is None:
+            idx = slice(None, None)
+        return np.exp(self.Elogb[idx]).dot(np.exp(self.Elogt))
+
+    def _bound(self, X, idx=None):
         if idx is None:
             idx = slice(None, None)
         # E_q [log p(y, lambda | theta, beta) - log q(lambda)]
-        bound = np.sum(special.gammaln(self.nu_lam) -
-                       self.nu_lam * np.log(self.rho_lam))
-        bound -= self.r * (np.log(self.Eb[idx].dot(self.Et))).sum()
-        if not aux_updated:
-            # if we don't update aux, then this term is not equal 0
-            bound += np.sum((self.rho_lam - self.r /
-                             self.Ebinvinv[idx].dot(self.Etinvinv) - 1)
-                            * self.Elam)
+        bound = np.sum(X * np.log(self._xexplog(idx=idx)))
+        bound += gamma_term(self.Elam, self.Eloglam, self.nu_lam, self.rho_lam,
+                            self.r + X, self.r + self.Eb[idx].dot(self.Et))
         # E_q [log p(theta) - log q(theta)]
-        bound += gig_gamma_term(self.Et, self.Etinv, self.rho_t, self.tau_t,
-                                self.a, self.b)
+        bound += gamma_term(self.Et, self.Elogt, self.nu_t, self.rho_t,
+                            self.a, self.b)
         # E_q [log p(beta) - log q(beta)]
-        bound += gig_gamma_term(self.Eb[idx], self.Ebinv[idx], self.rho_b[idx],
-                                self.tau_b[idx], self.c, self.d)
+        bound += gamma_term(self.Eb[idx], self.Elogb[idx], self.nu_b[idx],
+                            self.rho_b[idx], self.c, self.d)
         return bound
 
 
 class OnlineNegBinomMF(NegBinomMF):
     ''' Negtive-binomial matrix factorization with stochastic inference '''
     def __init__(self, n_components=100, batch_size=10, n_pass=10,
-                 max_inner_iter=20, tol=0.0005, shuffle=True, smoothness=100,
-                 random_state=None, verbose=False,
+                 max_inner_iter=20, shuffle=True, tol=0.0001, init_flat_lam=True,
+                 smoothness=100, random_state=None, verbose=False,
                  **kwargs):
         ''' Negative-binomial matrix factorization
         Arguments
@@ -250,11 +246,15 @@ class OnlineNegBinomMF(NegBinomMF):
             The number of passes through the entire data
         max_iter : int
             Maximal number of iterations to perform for a single mini-batch
-        tol : float
-            The threshold on the increase of the objective to stop the
-            iteration
+        max_inner_iter : int
+            Maximal number of iterations to update local factors
         shuffle : bool
             Whether to shuffle the data or not
+        init_temp : float
+            The initial tempurature for determinstic annealing
+        anneal_len : float
+            The annealing length, measured in effective traversals of the
+            dataset
         smoothness : int
             Smoothness on the initialization variational parameters
         random_state : int or RandomState
@@ -270,8 +270,9 @@ class OnlineNegBinomMF(NegBinomMF):
         self.n_pass = n_pass
         self.max_iter = DEFAULT_NUM_ITER
         self.max_inner_iter = max_inner_iter
-        self.tol = tol
         self.shuffle = shuffle
+        self.tol = tol
+        self.init_flat_lam = init_flat_lam
         self.smoothness = smoothness
         self.random_state = random_state
         self.verbose = verbose
@@ -289,7 +290,7 @@ class OnlineNegBinomMF(NegBinomMF):
         self.c = float(kwargs.get('c', 0.1))
         self.d = float(kwargs.get('d', 0.1))
 
-        self.r = float(kwargs.get('r', 2.0))
+        self.r = float(kwargs.get('r', 1.0))
 
         self.t0 = float(kwargs.get('t0', 1.))
         self.kappa = float(kwargs.get('kappa', 0.5))
@@ -328,7 +329,9 @@ class OnlineNegBinomMF(NegBinomMF):
                 print '\tMinibatch %d:' % i
                 idx = indices[istart: istart + self.batch_size]
                 mini_batch = X[idx]
-                self.set_learning_rate(mini_batch, iter=i)
+                if sparse.isspmatrix(mini_batch):
+                    mini_batch = mini_batch.toarray()
+                self.set_learning_rate(mini_batch, iter=count * n_items / self.batch_size + i)
                 self.partial_fit(mini_batch, idx=idx)
                 self.bound.append(self._stoch_bound(mini_batch, idx=idx))
         return self
@@ -347,18 +350,15 @@ class OnlineNegBinomMF(NegBinomMF):
         '''
         self.transform(X, idx=idx)
         # take a (natural) gradient step
-        EXinv = 1. / self.Eb[idx].dot(self.Et)
-        laminvXsq = self.Elam / (self.Ebinvinv[idx].dot(self.Etinvinv))**2
-        self.rho_t = (1 - self.rho) * self.rho_t + self.rho * \
-            (self.b + self._scale * self.r * self.Eb[idx].T.dot(EXinv))
-        self.tau_t = (1 - self.rho) * self.tau_t + self.rho * \
-            self._scale * self.r * self.Etinvinv**2 * \
-            self.Ebinvinv[idx].T.dot(laminvXsq)
-        self.tau_t[self.tau_t < 1e-100] = 0
 
-        self.Et, self.Etinv = comp_gig_expectations(self.a,
-                                                    self.rho_t,
-                                                    self.tau_t)
+        ratio = X / self._xexplog(idx=idx)
+        self.nu_t = (1 - self.rho) * self.nu_t + self.rho * \
+            (self.a + self._scale * np.exp(self.Elogt) * np.exp(self.Elogb[idx].T).dot(ratio))
+        self.rho_t = (1 - self.rho) * self.rho_t + self.rho * \
+            (self.b + self._scale * self.Eb[idx].T.dot(self.Elam))
+        old_Et = self.Et.copy()
+        self.Et, self.Elogt = comp_gamma_expectations(self.nu_t, self.rho_t)
+        print '\tEt relative change: %.3f' % np.sqrt(np.sum((self.Et - old_Et)**2))
         return self
 
     def set_learning_rate(self, X, iter=None, rho=None):
@@ -390,18 +390,17 @@ class OnlineNegBinomMF(NegBinomMF):
         if idx is None:
             idx = slice(None, None)
         # E_q [log p(y, lambda | theta, beta) - log q(lambda)]
-        bound = np.sum(special.gammaln(self.nu_lam) -
-                       self.nu_lam * np.log(self.rho_lam))
-        bound -= self.r * (np.log(self.Eb[idx].dot(self.Et))).sum()
+        bound = np.sum(X * np.log(self._xexplog(idx=idx)))
+        bound += gamma_term(self.Elam, self.Eloglam, self.nu_lam, self.rho_lam,
+                            self.r + X, self.r + self.Eb[idx].dot(self.Et))
         # E_q [log p(beta) - log q(beta)]
-        bound += gig_gamma_term(self.Eb[idx], self.Ebinv[idx], self.rho_b[idx],
-                                self.tau_b[idx], self.c, self.d)
+        bound += gamma_term(self.Eb[idx], self.Elogb[idx], self.nu_b[idx],
+                            self.rho_b[idx], self.c, self.d)
         # scale the objective to "pretent" we have the full dataset
         bound *= self._scale
         # E_q [log p(theta) - log q(theta)]
-        bound += gig_gamma_term(self.Et, self.Etinv, self.rho_t, self.tau_t,
-                                self.a, self.b)
-
+        bound += gamma_term(self.Et, self.Elogt, self.nu_t, self.rho_t,
+                            self.a, self.b)
         return bound
 
 
@@ -409,62 +408,10 @@ def comp_gamma_expectations(alpha, beta):
     return (alpha / beta, special.psi(alpha) - np.log(beta))
 
 
-def comp_gig_expectations(alpha, beta, gamma):
-    alpha = alpha * np.ones_like(beta)
-
-    Ex, Exinv = np.zeros_like(beta), np.zeros_like(beta)
-
-    # For very small values of gamma and positive values of alpha, the GIG
-    # distribution becomes a gamma distribution, and its expectations are both
-    # cheaper and more stable to compute that way.
-    gig_inds = (gamma > 1e-200)
-    gam_inds = (gamma <= 1e-200)
-
-    if np.any(alpha[gam_inds] < 0):
-        raise ValueError("problem with arguments.")
-
-    # Compute expectations for GIG distribution.
-    sqrt_beta = np.sqrt(beta[gig_inds])
-    sqrt_gamma = np.sqrt(gamma[gig_inds])
-    # Note that we're using the *scaled* version here, since we're just
-    # computing ratios and it's more stable.
-    bessel_alpha_minus = special.kve(alpha[gig_inds] - 1, 2 * sqrt_beta *
-                                     sqrt_gamma)
-    bessel_alpha = special.kve(alpha[gig_inds], 2 * sqrt_beta * sqrt_gamma)
-    bessel_alpha_plus = special.kve(alpha[gig_inds] + 1, 2 * sqrt_beta *
-                                    sqrt_gamma)
-    sqrt_ratio = sqrt_gamma / sqrt_beta
-
-    Ex[gig_inds] = bessel_alpha_plus * sqrt_ratio / bessel_alpha
-    Exinv[gig_inds] = bessel_alpha_minus / (sqrt_ratio * bessel_alpha)
-
-    # Compute expectations for gamma distribution where we can get away with
-    # it.
-    Ex[gam_inds] = alpha[gam_inds] / beta[gam_inds]
-    Exinv[gam_inds] = beta[gam_inds] / (alpha[gam_inds] - 1)
-    Exinv[Exinv < 0] = np.inf
-
-    return (Ex, Exinv)
-
-
-def gig_gamma_term(Ex, Exinv, rho, tau, a, b):
-    ''' Compute E_q[log p(x)] - E_q[log q(x)] where:
-        p(x) = Gamma(a, b), q(x) = GIG(a, rho, tau)
+def gamma_term(Ex, Elogx, nu, rho, a, b):
+    ''' Compute E_q[log p(x) - log q(x)] where
+    p(x) = Gamma(a, b) and q(x) = Gamma(nu, rho)
     '''
-    score = 0
-    cut_off = 1e-200
-    zero_tau = (tau <= cut_off)
-    non_zero_tau = (tau > cut_off)
-
-    score = score - np.sum((b - rho) * Ex)
-    score = score - np.sum(non_zero_tau) * log(.5)
-    score = score + np.sum(tau[non_zero_tau] * Exinv[non_zero_tau])
-    score = score + Ex.size * (a * log(b) - special.gammaln(a))
-    score = score - .5 * a * np.sum(np.log(rho[non_zero_tau]) -
-                                    np.log(tau[non_zero_tau]))
-    # It's numerically safer to use scaled version of besselk
-    score = score + np.sum(np.log(special.kve(
-        a, 2 * np.sqrt(rho[non_zero_tau] * tau[non_zero_tau]))) -
-        2 * np.sqrt(rho[non_zero_tau] * tau[non_zero_tau]))
-    score = score + np.sum(-a * np.log(rho[zero_tau]) + special.gammaln(a))
+    score = ((a - nu) * Elogx + (rho - b) * Ex + special.gammaln(nu)
+             - nu * np.log(rho)).sum()
     return score
